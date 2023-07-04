aac4e43ff20dfa50653e2c0460e71255de115f64
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-05 00:09:28 +0200

    Update Network adapter.ps1

--- Network adapter.ps1
+++ Network adapter.ps1
@@ -20,3 +20,10 @@ netsh wlan show interfaces
 netsh wlan show profiles
 
 netsh wlan show profile "konkretni wifi"
+
+#V sekci Security settings sice vidím detaily o zabezpečení, nikoliv však samotný klíč – heslo k Wi-Fi. 
+#Aby jej netsh zobrazil, příkaz opět kosmeticky upravím:
+netsh wlan show profile "NEXT 2018" key=clear
+
+#A je to, v sekci Security settings se nyní na konci zobrazila položka Key Content, která obsahuje heslo k této Wi-Fi:
+
