7cc7032961d23ca2b857b596c64eb709e15781ce
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-05 00:08:03 +0200

    Update Network adapter.ps1

--- Network adapter.ps1
+++ Network adapter.ps1
@@ -7,3 +7,16 @@ Disable-NetAdapter -Name "your network adapter name" -Confirm:$false
 #The undo command is 
 
 Enable-NetAdapter -Name "your network adapter name" -Confirm:$false
+
+# CMD COMMAND 
+netsh wlan show interfaces
+
+#Microsoft v průběhu let všemožně měnil způsob, jak se k tomuto nastavení dostat, a bez Googlu bych se tak už neobešel. 
+#Raději použiji jeden jediný příkaz v netsh – Network Shellu –, který je součástí operačního systému.
+
+#Interaktivní textový program dokáže rychle odhalit některé detaily o sítích, přičemž seznam těch bezdrátových si můžete 
+#v libovolné textovém terminálu pro Windows zobrazit příkazem:
+
+netsh wlan show profiles
+
+netsh wlan show profile "konkretni wifi"
